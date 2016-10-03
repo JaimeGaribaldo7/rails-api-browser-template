31f5130d484cef407d23b5a28e56b9efa2c6debe
JaimeGaribaldo7 <jaimegaribaldo753@gmail.com>
2016-10-03 10:28:46 -0400

    Add files for Forms.
    Forms working!

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,10 @@
 
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
+const authEvents = require('./events.js');
 
+$(() => {
+  authEvents.addHandlers();
+});
 // use require without a reference to ensure a file is bundled
 require('./example');
---
+++ assets/scripts/ui.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const app = require('./app.js');
+
+const success = (data) => {
+  console.log(data);
+};
+
+const changePasswordSuccess = () => {
+  console.log('password successfully changed');
+};
+
+const failure = (error) => {
+  console.log(error);
+};
+
+const signInSuccess = (data) => {
+  app.user = data.user;
+};
+
+const signOutSuccess = () => {
+
+  app.user = null;
+};
+
+
+module.exports = {
+  failure,
+  success,
+  signInSuccess,
+  changePasswordSuccess,
+  signOutSuccess,
+};
